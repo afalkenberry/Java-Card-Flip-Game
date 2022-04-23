7d654123a15ffcb41ece1fe8adc3718501e7dbda
afalkenberry <101435646+afalkenberry@users.noreply.github.com>
2022-04-22 20:54:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //this file contains JavaScript code for the card flip game
+
+function createNewCard() {
+	let cardElement = document.createElement("div");
+	cardElement.classList.add("card");
+  cardElement.innerHTML=`<div class="card-down"></div>
+		<div class="card-up"></div>`;
+	return cardElement;
+}
+
+function appendNewCard(parentElement) {
+	let cardElement = createNewCard();
+	parentElement.appendChild(cardElement);
+	return cardElement;
+}
+
+function shuffleCardImageClasses() {
+	const cardClasses = ["image-1", "image-1", "image-2", "image-2", "image-3", "image-3", "image-4", "image-4", "image-5", "image-5", "image-6", "image-6"]
+  let result = _.shuffle(cardClasses) // (_.shuffle) from underscore.js
+  return result
+}
+
